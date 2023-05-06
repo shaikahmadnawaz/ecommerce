5793c211c73b833e3b724edf12fba0a259ac3f8a
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-05-06 12:37:07 +0530

    read

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+let products = [
+  { id: 1, name: "Product 1", price: 10.99 },
+  { id: 2, name: "Product 2", price: 19.99 },
+];
+
+// get all products
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+// get a single product
+app.get("/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.find((product) => product.id === productId);
+  if (!product) {
+    res.status(404).json({ message: "Product not found" });
+  } else {
+    res.json(product);
+  }
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
