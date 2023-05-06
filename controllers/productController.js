b8571bd9dda57f621725ffa0a98f99a6cf1c3273
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-05-06 18:30:35 +0530

    products routes and controller completed

---
+++ controllers/productController.js
@@ -0,0 +1,80 @@
+import Product from "../models/Product.js";
+
+// Get all products
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Get a single product by ID
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Create a new product
+const createProduct = async (req, res) => {
+  const { name, price, description } = req.body;
+  try {
+    const product = await Product.create({ name, price, description });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Update a product
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, description } = req.body;
+  try {
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { name, price, description },
+      { new: true }
+    );
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Delete a product
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- server.js
+++ server.js
@@ -4,67 +4,14 @@ import connectDB from "./db/connectDB.js";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-let products = [
-  { id: 1, name: "Product 1", price: 10.99 },
-  { id: 2, name: "Product 2", price: 19.99 },
-];
-
-// get all products
-app.get("/products", (req, res) => {
-  res.json(products);
-});
-
-// get a single product
-app.get("/products/:id", (req, res) => {
-  const productId = parseInt(req.params.id);
-  const product = products.find((product) => product.id === productId);
-  if (!product) {
-    res.status(404).json({ message: "Product not found" });
-  } else {
-    res.json(product);
-  }
-});
-
-// create a new product
-app.post("/products", (req, res) => {
-  const { id, name, price } = req.body;
-  const newProduct = { id, name, price };
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
-
-// update a product
-app.patch("/products/:id", (req, res) => {
-  const productId = parseInt(req.params.id);
-  const { name, price } = req.body;
-  const product = products.find((product) => product.id === productId);
-  if (!product) {
-    res.status(404).json({ message: "Product not found" });
-  } else {
-    product.name = name || product.name;
-    product.price = price || product.price;
-    res.json(product);
-  }
-});
-
-// delete a product
-app.delete("/products/:id", (req, res) => {
-  const productId = parseInt(req.params.id);
-  const productIndex = products.findIndex(
-    (product) => product.id === productId
-  );
-  if (productIndex === -1) {
-    res.status(404).json({ message: "Product not found" });
-  } else {
-    const deletedProduct = products.splice(productIndex, 1)[0];
-    res.json(deletedProduct);
-  }
-});
+// Use the product routes
+app.use("/products", productRoutes);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
